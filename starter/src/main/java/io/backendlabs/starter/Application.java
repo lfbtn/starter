6a3ec4703fcab691bdd7cd9df45f4232ce743d33
luizbotton <31282962+luizbotton@users.noreply.github.com>
2019-09-27 10:57:08 -0300

    initial

---
+++ starter/src/main/java/io/backendlabs/starter/Application.java
@@ -0,0 +1,22 @@
+package io.backendlabs.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	private static Class applicationClass = Application.class;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+}
